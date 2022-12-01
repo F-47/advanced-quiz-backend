4c73de6a1388afef3504a2f745f9bd3952b299cd
Executioner47 <faresgalal09@gmail.com>
2022-12-01 23:24:15 +0200

    second commit

--- app.js
+++ app.js
@@ -2,26 +2,24 @@ let express = require('express');
 let mongoose = require('mongoose')
 let bodyParser = require('body-parser');
 const quizController = require("./Controllers/quizController");
+require('dotenv').config()
 let app = express();
 
 app.use(bodyParser.json());
 
 //create mongodb
-let dbURI = 'mongodb+srv://fares:Abomadawy12@Quizes.jnwjcua.mongodb.net/QuizApp'
+let dbURI = process.env.MONGO_DB_URL
 mongoose.connect(dbURI)
     .then(() => console.log("Database connected!"))
     .catch((err)=>console.log(err))
-
-//register view engine
-app.set('view engine', 'ejs') // app.set('views','views2') // to change directory 
 
 //middleware and static files
 app.use(express.static('public')) // we can access any file in the public folder
 app.use(express.urlencoded({extended:true})) // so we can access the data coming from the input value
 
-app.get("/getAllQuizes", quizController.getAllQuizes);
+app.get("/quiz", quizController.getAllQuizes);
 app.get("/quiz/:id", quizController.getQuizById);
-app.post('/createQuiz', quizController.quizCreatePost)
+app.post('/quiz', quizController.quizCreatePost)
 
 app.listen(4000, () => {
     console.log(`conected with port 4000`);
